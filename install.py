81c5132cc2beb2f90ed7aa2f4b7e21f172cdeb42
overflow <err.overflow@gmail.com>
2025-03-07 22:28:35 +0800

    feat: 添加代理功能和自定义动作，包括Fishing、ScreenShot和ShotSelf

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 from pathlib import Path
+
 import PyInstaller.__main__
-import site
-import os
 
 import shutil
 import sys
@@ -11,49 +10,30 @@ from configure import configure_ocr_model
 
 
 working_dir = Path(__file__).parent
-install_path = working_dir / Path("maapicli")
+install_path = working_dir / Path("install")
 version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
 
 
-def bulid():
-    # 获取 site-packages 目录列表
-    site_packages_paths = site.getsitepackages()
+def install_deps():
+    if not (working_dir / "deps" / "bin").exists():
+        print('Please download the MaaFramework to "deps" first.')
+        print('请先下载 MaaFramework 到 "deps"。')
+        sys.exit(1)
 
-    # 查找包含 MaaAgentBinary 的路径
-    maa_bin_path2 = None
-    for path in site_packages_paths:
-        potential_path = os.path.join(path, "MaaAgentBinary")
-        if os.path.exists(potential_path):
-            maa_bin_path2 = potential_path
-            break
-
-    if maa_bin_path2 is None:
-        raise FileNotFoundError("not found MaaAgentBinary")
-
-    # 构建 --add-data 参数
-    add_data_param2 = f"{maa_bin_path2}{os.pathsep}MaaAgentBinary"
-
-    command = [
-        "maapicli.py",
-        "--name=maapicli",
-        f"--add-data={add_data_param2}",
-        f"--distpath={working_dir}",
-        "--clean",
-    ]
-    PyInstaller.__main__.run(command)
-
-    maa_bin_path = None
-    for path in site_packages_paths:
-        potential_path = os.path.join(path, "maa", "bin")
-        if os.path.exists(potential_path):
-            maa_bin_path = potential_path
-            break
-
-    if maa_bin_path is None:
-        raise FileNotFoundError("not found maa/bin")
     shutil.copytree(
-        maa_bin_path,
+        working_dir / "deps" / "bin",
         install_path,
+        ignore=shutil.ignore_patterns(
+            "*MaaDbgControlUnit*",
+            "*MaaThriftControlUnit*",
+            "*MaaRpc*",
+            "*MaaHttp*",
+        ),
+        dirs_exist_ok=True,
+    )
+    shutil.copytree(
+        working_dir / "deps" / "share" / "MaaAgentBinary",
+        install_path / "MaaAgentBinary",
         dirs_exist_ok=True,
     )
 
@@ -63,11 +43,14 @@ def install_resource():
     configure_ocr_model()
 
     shutil.copytree(
-        working_dir / "assets" ,
-        install_path ,
+        working_dir / "assets" / "resource",
+        install_path / "resource",
         dirs_exist_ok=True,
     )
-    shutil.rmtree(install_path / "MaaCommonAssets")
+    shutil.copy2(
+        working_dir / "assets" / "interface.json",
+        install_path,
+    )
 
     with open(install_path / "interface.json", "r", encoding="utf-8") as f:
         interface = json.load(f)
@@ -89,7 +72,26 @@ def install_chores():
     )
 
 
+def install_agent():
+    # 名字为agent
+    command = [
+        str(working_dir / "agent" / "main.py"),
+        "--name=agent",
+        "--clean",
+    ]
+    PyInstaller.__main__.run(command)
+    print(working_dir / "dist" / "agent")
+    shutil.copytree(
+        working_dir / "dist" / "agent",
+        install_path / "agent",
+        dirs_exist_ok=True,
+    )
+
+
 if __name__ == "__main__":
-    bulid()
+    install_deps()
     install_resource()
     install_chores()
+    install_agent()
+
+    print(f"Install to {install_path} successfully.")
