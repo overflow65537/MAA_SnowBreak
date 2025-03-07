fca1f4ef251107ea4674349463b8ab7c31577023
overflow <err.overflow@gmail.com>
2025-03-07 21:43:34 +0800

    refactor:使用Agent重构custom程序

---
+++ assets/agent/action/Fishing.py
@@ -0,0 +1,31 @@
+from maa.agent.agent_server import AgentServer
+from maa.context import Context
+from maa.custom_action import CustomAction
+import time
+
+@AgentServer.custom_action("Fishing")
+class Fishing(CustomAction):
+    def run(
+        self, context: Context, argv: CustomAction.RunArg
+    ) -> CustomAction.RunResult:
+        image = context.tasker.controller.post_screencap().wait().get()
+        result = context.run_recognition("检查黄色块数量", image)
+
+        if result.best_result.count:
+            initial_yellow_count = result.best_result.count
+            yellow_count = result.best_result.count
+
+        start_time = time.time()
+        while time.time() - start_time < 2:
+            if yellow_count < initial_yellow_count - 50:
+                break
+
+            image = context.tasker.controller.post_screencap().wait().get()
+            result = context.run_recognition("检查黄色块数量", image)
+            if not result:
+                return CustomAction.RunResult(success=True)
+            yellow_count = result.best_result.count
+
+        context.tasker.controller.post_click(1171, 618).wait()
+
+        return CustomAction.RunResult(success=True)
---
+++ assets/agent/main.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from pathlib import Path
+print(sys.argv)
+if len(sys.argv) > 2:
+    binding_dir = Path(sys.argv[-2]).resolve()
+    os.environ["MAAFW_BINARY_PATH"] = str(binding_dir)
+    print(f"binding_dir: {binding_dir}")
+
+from maa.agent.agent_server import AgentServer
+from maa.toolkit import Toolkit
+
+from action.Fishing import Fishing
+from action.ScreenShot import ScreenShot
+from action.ShotSelf import ShotSelf
+from action.ShotTarget import ShotTarget
+
+def main():
+    Toolkit.init_option("./")
+    if len(sys.argv) >1:
+        socket_id = sys.argv[-1]
+    else:
+        socket_id = "MAA_AGENT_SOCKET"
+    AgentServer.start_up(socket_id)
+    AgentServer.join()
+    AgentServer.shut_down()
+
+if __name__ == "__main__":
+    main()
