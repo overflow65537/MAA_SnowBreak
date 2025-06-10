9985108b8d98ae30a4e82cd9d976ae6bc57723a1
overflow65537 <err.overflow@gmail.com>
2025-06-10 22:41:26 +0800

    feat: 更新分辨率检查流程，添加错误提示和后续步骤

--- assets/custom/Recognition/CheckResolution.py
+++ assets/custom/Recognition/CheckResolution.py
@@ -24,10 +24,10 @@ MAA_SnowBreak 分辨率检查
 作者:overflow65537
 """
 
+from funkify import T
 from maa.context import Context
 from maa.custom_recognition import CustomRecognition
-import os
-import chardet
+from numpy import ndarray
 
 
 class CheckResolution(CustomRecognition):
@@ -37,59 +37,19 @@ class CheckResolution(CustomRecognition):
         argv: CustomRecognition.AnalyzeArg,
     ) -> CustomRecognition.AnalyzeResult | None:
         try:
-            log_path = os.path.join(os.getcwd(), "debug", "maa.log")
-            # 读取文件的前部分内容以检测编码
-            with open(log_path, "rb") as raw_file:
-                raw_data = raw_file.read(1024)
-                result = chardet.detect(raw_data)
-                encoding = result["encoding"]
+            image:ndarray = argv.image # type: ignore
+            height, width = image.shape[:2]
 
-            # 使用检测到的编码读取文件
-            with open(log_path, "r", encoding=encoding, errors="replace") as file:
-                lines = file.readlines()
-                # 倒序
-                lines.reverse()
-                for line in lines:
-                    if (
-                        "[MtouchHelper.cpp][L29][MaaNS::CtrlUnitNs::MtouchHelper::read_info]"
-                        in line
-                    ):
-                        # 提取 分辨率
-                        parts = line.split()
-                        width = int(parts[-3])
-                        height = int(parts[-2])
-
-                        # 计算比例是否为 16:9
-                        ratio = width / height
-                        target_ratio = 16 / 9
-                        is_16_9 = abs(ratio - target_ratio) < 0.001
-                        if is_16_9:
-                            return CustomRecognition.AnalyzeResult(
-                                box=[10, 10, 0, 0],
-                                detail=f"分辨率为 {width}x{height}, 比例为 16:9",
-                            )
-                    elif "[MtouchHelper.cpp][L53][MaaNS::CtrlUnitNs::MtouchHelper::read_info]" in line:
-                        width_start = line.find("display_width_=") + len("display_width_=")
-                        width_end = line.find("]", width_start)
-                        width = int(line[width_start:width_end])
-
-                        height_start = line.find("display_height_=") + len("display_height_=")
-                        height_end = line.find("]", height_start)
-                        height = int(line[height_start:height_end])
-                        # 计算比例是否为 16:9
-                        ratio = width / height
-                        target_ratio = 16 / 9
-                        is_16_9 = abs(ratio - target_ratio) < 0.001
-                        if is_16_9:
-                            return CustomRecognition.AnalyzeResult(
-                                box=[10, 10, 0, 0],
-                                detail=f"分辨率为 {width}x{height}, 比例为 16:9",
-                            )
-                    else:
-                        continue
-            
-
-            return None
-
+            ratio = width / height
+            target_ratio = 16 / 9
+            is_16_9 = abs(ratio - target_ratio) < 0.001
+            if not is_16_9:
+                return CustomRecognition.AnalyzeResult(
+                box=[0, 0, 0, 0],
+                detail=f"分辨率{width}x{height} 错误",
+            )
+            else:
+                return None
         except Exception as e:
+            print(f"Error in CheckResolution: {e}")
             return None
