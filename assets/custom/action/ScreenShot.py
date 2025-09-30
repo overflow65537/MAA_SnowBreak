4fec641fcdab0725f06efc970d3e03e2b64e178e
ERR_Overflow <err.overflow@gmail.com>
2025-09-30 20:59:30 +0800

    fix:修复截图进程输入参数错误

--- assets/custom/action/ScreenShot.py
+++ assets/custom/action/ScreenShot.py
@@ -38,7 +38,9 @@ class ScreenShot(CustomAction):
     def run(
         self, context: Context, argv: CustomAction.RunArg
     ) -> CustomAction.RunResult:
-        argv: dict = json.loads(argv.custom_action_param)
+        param: dict = json.loads(argv.custom_action_param)
+        if param is None:
+            param = {}
         image: numpy.ndarray = context.tasker.controller.post_screencap().wait().get()
 
         debug_dir = os.path.abspath("debug")
@@ -57,7 +59,7 @@ class ScreenShot(CustomAction):
 
         height, width, _ = image.shape
         current_time = (
-            argv.get("type", "") + "_" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".png"
+            param.get("type", "") + "_" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".png"
         )
         debug_path = os.path.join("debug", current_time)
 
